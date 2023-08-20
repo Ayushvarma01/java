7db3a94fec4efac00273f7cff8c8bef94e163313
ayush Varma <99134940+Ayushvarma01@users.noreply.github.com>
2023-08-20 19:44:20 +0530

    Add files via upload

---
+++ array/How_to_sort_int_number.java
@@ -0,0 +1,23 @@
+package important_program;
+
+public class How_to_sort_int_number {
+	public static void main(String[] args) {
+		int number = 312;
+		char temp = '0';
+		String num = Integer.toString(number);
+		char ch[] = num.toCharArray();
+		for (int i = 0; i < ch.length; i++) {
+			for (int j = i+1; j < ch.length; j++) {
+				if (ch[j] < ch[i]) {
+					temp = ch[j];
+					ch[j] = ch[i];
+					ch[i] = temp;
+				}
+			}
+		}
+		for (char s : ch) {
+			System.out.println(s);
+		}
+//		System.out.println(temp);
+	}
+}
