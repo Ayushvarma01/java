8d1da5ff65a6d799f1be19e60b149d27c7cfd35e
ayush Varma <99134940+Ayushvarma01@users.noreply.github.com>
2023-08-25 07:04:21 +0530

    Add files via upload

---
+++ array/Remove_digit.java
@@ -0,0 +1,24 @@
+package JDBC;
+
+import java.util.*;
+import java.text.*;
+
+public class Remove_digit {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter your input ");
+		String input = scanner.nextLine();//taking input from user and storing into input variable  
+
+		String temp = new String();//taking new object String to store new String after loop
+		for (int i = 0; i < input.length(); i++) {//looping input string 
+			char ch = input.charAt(i);// converting input String into char array format
+
+			if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z') || ( ch==' ')) {//checking if ch array have a to z( A to Z ) character into char array or not  
+				temp += ch;//and storing only character into temp object
+			}
+
+		}
+		System.out.println(temp);//and finally printing temp variable to user console
+	}
+
+}
